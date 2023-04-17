3c6f158711cf85844f6f545df48effc70c304d99
Bali Tok <bali.tok@hotmail.com>
2023-04-17 11:16:15 +0800

    add background color to form page

--- problem2/src/components/Form.js
+++ problem2/src/components/Form.js
@@ -107,7 +107,7 @@ export default function Form() {
           />
         </div>
         <div className="order-first sm:order-last">
-          <section className="bg-gray-50 dark:bg-gray-900">
+          <section className="bg-switcheo">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
               <a
                 href="https://www.switcheo.com/"
@@ -122,7 +122,7 @@ export default function Form() {
                 />
                 Switcheo
               </a>
-              <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="w-full bg-white border border-solid border-gray-400 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                   <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Transaction
--- problem2/tailwind.config.js
+++ problem2/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
           800: "#1e40af",
           900: "#1e3a8a",
         },
+        switcheo: "#e3fca4",
       },
     },
     fontFamily: {
